45f60b80c3b3109d67e80b5787f74bfcbc44dab1
g33kgrrl <exexpat2@gmail.com>
2014-12-24 15:55:01 -0800

    Revert clobbering of earlier bg_form changes

--- templates/bg_form.php
+++ templates/bg_form.php
@@ -1,5 +1,8 @@
 <div>
-    <h2><?= $_SESSION["username"] . "'s Blood Glucose" ?></h2>
+
+
+<div>
+    <h2><?=$title?></h2>
 
     <form action="bg.php" method="post">
         <fieldset>
@@ -7,39 +10,65 @@
             <div class="bgEntry">
                 <input autofocus class="form-small" name="bgReading" placeholder="###" type="number" min="1" max="999" value="" required /> mg/dL
             </div>
-            
+
             <div>
                 <table class="bgTable">
+                
+                <?php
+                    $mealtimes = [ 'F', 'BB', 'AB', 'BL', 'AL', 'BD', 'AD', 'B', 'R' ];
+                    $longnames = [ 'Fasting', 'Before Breakfast', 'After Breakfast', 'Before Lunch',
+                        'After Lunch', 'Before Dinner', 'After Dinner', 'Bedtime', 'Random' ];
+                        
+                    $cnt = 2;
+                    
+                    foreach ($mealtimes as $idx => $mealtime) {
+                        if (strlen($mealtime) === 1) {
+                ?>
+                    
                     <tr>
-                        <td colspan="2" class="F center"><input type="radio" name="mealtime" id="F" value="F" required> 
-                        <label for="F">Fasting</label></td>
+                        <td colspan="2" class="<?=$mealtime?> center">
+                    
+                <?php
+                            $cnt = 2;
+                        } else {
+                            if ($cnt > 1) {
+                ?>
+                    
+                    <tr>
+                
+                <?php
+                            $cnt = 0;
+                        }
+                ?>
+                
+                        <td class="<?=$mealtime?>">
+                
+                <?php
+                            ++$cnt;
+                        }
+                ?>
+                            <input type="radio" name="mealtime" id="<?=$mealtime?>" value="<?=$mealtime?>" required />
+                            <label for="<?=$mealtime?>"><?=$longnames[$idx]?></label>
+                        </td>
+
+                <?php
+                        if ($cnt > 1) {
+                ?>
+                
                     </tr>
-                    <tr>
-                        <td class="BB"><input type="radio" name="mealtime" id="BB" value="BB" required> <label for="BB">Before Breakfast</label></td>
-                        <td class="AB"><input type="radio" name="mealtime" id="AB" value="AB" required> <label for="AB"> After Breakfast</label></td>
-                    </tr>
-                    <tr>
-                        <td class="BL"><input type="radio" name="mealtime" id="BL" value="BL" required> <label for="BL"> Before Lunch</label></td>
-                        <td class="AL"><input type="radio" name="mealtime" id="AL" value="AL" required> <label for="AL">After Lunch</label></td>
-                    </tr>
-                    <tr>
-                        <td class="BD"><input type="radio" name="mealtime" id="BD" value="BD" required> <label for="BD">Before Dinner</label></td>
-                        <td class="AD"><input type="radio" name="mealtime" id="AD" value="AD" required> <label for="AD">After Dinner</label></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="B center"><input type="radio" name="mealtime" id="B" value="B" required> <label for="B">Bedtime</label></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" class="R center"><input type="radio" name="mealtime" id="R" value="R" required> <label for="R">Random</label></td>
-                    </tr>                    
+                
+                <?php
+                        }
+                    }
+                ?>
+
                 </table>
-            
             </div>            
+
             <div class="form-group">
                 <button type="submit" class="btn btn-default">Submit</button>
             </div>
-                           
+            
         </fieldset>
     </form>
 </div>
-
